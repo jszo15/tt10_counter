03c7b1ebeab28cc391606b1b75c0c9c953bd8066
jszo15 <121969363+jszo15@users.noreply.github.com>
2025-08-19 15:50:59 -0600

    Update tt_um_counter.v

--- src/tt_um_counter.v
+++ src/tt_um_counter.v
@@ -24,7 +24,7 @@ module tt_um_counter (
     );  
     
   // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out[7:4)  = 4'b0000;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uo_out[7:4]  = 4'b0000;  // Example: ou_out is the sum of ui_in and uio_in
     assign uio_out[7:0] = 8'b00000000;
   assign uio_oe[7:0] = 8'b00000000;
 
